b6338a795fb01ff357207d8dd5f53b8ed4fd5f76
ABC <abc@gmail.com>
2020-09-28 18:02:12 +0530

    UC 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("At start position that is 0");
+        }
+    }
+}
