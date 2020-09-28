c5e8101678b11aa3ae18c50fab343554b86b79b8
ABC <abc@gmail.com>
2020-09-28 19:21:34 +0530

    UC 6

--- Program.cs
+++ Program.cs
@@ -8,37 +8,50 @@ namespace start
     {
         static void Main(string[] args)
         {
-            //public const no_play = 1;
+            public const no_play = 1;
             public const ladder = 2;
-            //public const snake = 3;
+            public const snake = 3;
+            int curr_pos = 0;
             Random random = new Random();
-            int option = Random(1, 4);
-            /*switch(option)
+            int play = 1;
+            int sum = 0;
+            while(play ==1)
             {
-                  case no_play:
-                       Console.WriteLine("No Play");
-                       break;
-                  case ladder:
-                       Console.WriteLine("moves ahead by the number of position received in the die");
-                       break;
-                  case snake:
-                       Console.WriteLine("moves behind by the number of position received in the die");
-                       break;
-                  default:
+                int die = Random(1, 7);
+                int option = Random(1, 4);
+                switch(option)
+                {
+                    case no_play:
+                         Console.WriteLine("No Play");
+                         break;
+                    case ladder:
+                         curr_pos = curr_pos + die;
+                         Console.WriteLine("Current Position"+curr_pos);
+                         if (curr_pos>100)
+                         {
+                              curr_pos = 100;
+                         }
+                         sum++;
+                         break;
+                    case snake:
+                         curr_pos = curr_pos - die;
+                         Console.WriteLine("Current Position"+curr_pos);
+                         if (curr_pos<0)
+                         {
+                              curr_pos = 0;
+                         }
+                         sum++;
+                         break;
+                    default:
                        Console.WriteLine("Roll the die");
                        break;
-            }*/
-            if (option == ladder)
-            {
-                  int die = Random(1, 7);
-                  int curr_pos = Random(1, 101);
-                  if (((curr_pos)+(die))<0)
-                  {
-                       curr_pos = 0;
-                  }
-
+                }
+                if (curr_pos == 0 || curr_pos == 100)
+                {
+                      play = 0;
+                }
             }
-
+            Console.WriteLine("Total moves:" + sum);    
         }
     }
 }
