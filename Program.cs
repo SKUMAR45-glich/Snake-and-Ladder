936f620e919202b6e886431893b1b57b14c79cc1
ABC <abc@gmail.com>
2020-09-28 18:37:34 +0530

    UC 3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 
 namespace start
 {
@@ -6,9 +8,26 @@ namespace start
     {
         static void Main(string[] args)
         {
+            public const no_play = 1;
+            public const ladder = 2;
+            public const snake = 3;
             Random random = new Random();
-            int no_on_die = random.Next(1,7);
-            Console.WriteLine("Number on die is" + no_on_die);
+            int option = Random(1, 4);
+            switch(option)
+            {
+                  case no_play:
+                       Console.WriteLine("No Play");
+                       break;
+                  case ladder:
+                       Console.WriteLine("moves ahead by the number of position received in the die");
+                       break;
+                  case snake:
+                       Console.WriteLine("moves behind by the number of position received in the die");
+                       break;
+                  default:
+                       Console.WriteLine("Roll the die");
+                       break;
+            }
         }
     }
 }
