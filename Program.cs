68c3ac600ba72129e2028f3b22a1115305fe4727
SKUMAR45-glich <71806111+SKUMAR45-glich@users.noreply.github.com>
2020-10-05 12:45:52 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,33 +1,60 @@
-﻿using System;
-using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
+using System;
 
-namespace start
+namespace SnakeLadderGame
 {
     class Program
     {
         static void Main(string[] args)
         {
-            public const no_play = 1;
-            public const ladder = 2;
-            public const snake = 3;
-            Random random = new Random();
-            int option = Random(1, 4);
-            switch(option)
+            Console.WriteLine("Welcome to Snake and Ladder Game");
+            
+            int playerScore;
+
+            playerScore = 0;
+            Console.WriteLine("Initial Player Score : " + playerScore);
+
+            int currentScore;
+            currentScore = RollTheDie();
+            Console.WriteLine("The die rolled to " + currentScore);
+
+            string option;
+            option = CheckOptions();
+            Console.WriteLine("We got the option " + option);
+
+            switch (option)
             {
-                  case no_play:
-                       Console.WriteLine("No Play");
-                       break;
-                  case ladder:
-                       Console.WriteLine("moves ahead by the number of position received in the die");
-                       break;
-                  case snake:
-                       Console.WriteLine("moves behind by the number of position received in the die");
-                       break;
-                  default:
-                       Console.WriteLine("Roll the die");
-                       break;
+                case "NoPlay":
+                    break;
+                case "Ladder":
+                    playerScore += currentScore;
+                    break;
+                case "Snake":
+                    playerScore -= currentScore;
+                    break;
+                default:
+                    break;
             }
+            Console.WriteLine("Score After rolling the dice is " + playerScore);
+
+            return;
+        }
+
+        static int RollTheDie()
+        {
+            Random random = new Random();
+            int score = random.Next(1, 7);
+            return score;
+        }
+
+        static string CheckOptions()
+        {
+            string[] options = new string[3] { "NoPlay", "Ladder", "Snake" };
+            int idx;
+
+            Random random = new Random();
+            idx = random.Next(0, 3);
+
+            return options[idx];
         }
     }
 }
